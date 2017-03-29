6f6cd418f6303593b8ae6e914f94cb596bf3b49d
Евгений Лебедев <jony.kholukhoev@gmail.com>
2017-03-29 22:34:13 +0300

    done

--- src/HW_3/3_3.kt
+++ src/HW_3/3_3.kt
@@ -1,5 +1,7 @@
 package HW_3
 
+import java.util.*
+
 /**
  * Задача 3-3: (3 балла)
 
@@ -14,24 +16,12 @@ package HW_3
  */
 
 fun main(args: Array<String>) {
-    val list: List<Int> = listOf(1, 2, 1, 8, 2, 3, 4, 3, 4, 5, 6, 5, 6, 7, 2)
-    val set: Set<Int> = setOf(1, 2, 5, 8)
+    val list = listOf(1, 2, 3, 2, 2, 4, 6)
+    val set= setOf(1, 2, 4)
 
-    var result = Array(set.size) { IntArray(list.size) }
+    val res = mutableListOf<MutableList<Int>>()
 
-    for (i in 0..result.size - 1)
-        for (j in 0..result.size - 1)
-            result[i][j] = -1
-    set.elementAt(1)
 
-    for (i in 0..set.size - 1) {
-        for (j in 0..list.size - 1)
-            if (list[j] == set.elementAt(i))
-                result[i][j] = j
-    }
 
-    result.forEach {
-        it.filter { it > 0 }.forEach { print("$it ") }
-        print("\n")
-    }
+
 }
